26d39e99b4740ecff6889d681b95be488a1c2cf3
jeikabu <jeikabu@users.noreply.github.com>
2021-02-04 19:50:38 +0200

    Try fixing publish workflow

--- scripts/nupkg.ps1
+++ scripts/nupkg.ps1
@@ -1,8 +1,9 @@
 param(
     [string]$Version,
     [string]$NugetApiKey = "",
-    [string]$Nuget = "",
-    [string]$Pfx = ""
+    [string]$Nuget = "nuget",
+    [string]$CertFile = "",
+    [string]$CertBase64 = ""
 )
 
 $ErrorActionPreference = "Stop"
@@ -17,16 +18,31 @@ dotnet pack --configuration Release -p:Version=$Version
 
 $packages = Get-ChildItem $nupkgs
 
-if ($Pfx) {
-    if (-not $(Test-Path $Pfx -PathType Leaf)) {
-        throw "Need pfx"
-    }    
-    if (-not $(Test-Path $Nuget -PathType Leaf) -and -not $(Get-Command nuget -ErrorAction Ignore)) {
-        throw "Nuget executable not found"
+if ($CertFile -or $CertBase64) {
+    # If $Nuget isn't a path to a file and isn't a command in $PATH, download latest stable nuget.exe
+    if (-not $(Test-Path $Nuget -PathType Leaf) -and -not $(Get-Command $Nuget -ErrorAction Ignore)) {
+        $Nuget = "./nuget.exe"
+        Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile $Nuget
     }
-    foreach ($pkg in $packages) {
-        & $Nuget sign $pkg -Timestamper http://sha256timestamp.ws.symantec.com/sha256/timestamp -CertificatePath $Pfx
+    
+    try {
+        # If $Cert isn't a file assume it's a base64-encoded code-signing certificate
+        $tempCert = $null
+        if (-not $(Test-Path $CertFile -PathType Leaf)) {
+            $tempCert = New-TemporaryFile
+            [IO.File]::WriteAllBytes($tempCert.FullName, [Convert]::FromBase64String($CertBase64))
+            $CertFile = $tempCert.FullName
+        }
+        
+        foreach ($pkg in $packages) {
+            & $Nuget sign $pkg -Timestamper http://sha256timestamp.ws.symantec.com/sha256/timestamp -CertificatePath $CertFile
+        }
+    } finally {
+        if ($tempCert) {
+            $tempCert.Delete()
+        }
     }
+    
 }
 
 if ($NugetApiKey) {
